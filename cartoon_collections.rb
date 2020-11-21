30a5e004f01d4bd79cf17b4c513671824bb54a03
Kymani Davis <Kymanidavis@gmail.com>
2020-11-21 16:17:44 -0500

    commit tree c99c24243bc386e6dfb8396d2f9e8e3aff7cee00

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index do |object, index|
+    indexplus = index + 1
+    print "#{indexplus}#{object}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(let_our_powers_combine)
+  let_our_powers_combine.collect { |phrase| phrase.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+  
+def long_planeteer_calls(summon)
+  summon.any? do |phrase| 
+    phrase.length > 4
+  end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+end
+    
+
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.find do |type|
+    cheese_types.include?(type)
+  end
+  # the array below is here to help
+  #
 end
